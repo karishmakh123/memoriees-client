5723fb053cd11b1ba8569da49607c4a69e2f005f
karishmakh <your@email.com>
2021-09-29 13:03:22 +0530

    exit
    w

--- src/App.js
+++ src/App.js
@@ -1,25 +1,97 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React, { useState, useEffect} from 'react'
+import "./App.css";
+import Axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [name,setName] = useState("");
+    const [age,setAge] = useState(0);
+    const [ListOfFriends, setListOfFriends] = useState([]);
+
+    const addFriend = ()=>{
+        Axios.post("http://localhost:3001/addfriend",{ 
+            name:name, 
+            age:age, 
+           }).then ((response) =>{
+               setListOfFriends([...ListOfFriends,{_id:response.data._id,name:name,age:age}]);
+           }); 
+    };
+
+    const updateFriend = (id) => {
+       const newAge = prompt("Enter new age: ")
+
+       Axios.put("http://localhost:3001/update",{
+           newAge:Number(newAge),
+           id:id        
+       }).then(()=>{
+           setListOfFriends(ListOfFriends.map((val)=>{
+               
+               return val._id === id ?{_id:id, name:val.name, age:newAge}:val;
+                
+           }))
+       })
+    };
+
+    const deleteFriend=(id)=>{
+         Axios.delete(`http://localhost:3001/delete/${id}`).then(()=>{
+             setListOfFriends(ListOfFriends.filter((val) => {
+                
+                return val._id !== id;
+             }));
+         });
+    };
+
+    useEffect(()=>{
+        Axios.get("http://localhost:3001/read")
+            .then((response) =>{
+                setListOfFriends(response.data);
+            })
+            .catch(()=>{
+                console.log("ERR");
+            });
+
+    }, [] ); 
+
+    return (
+        <div className ="App">
+        <div className = "inputs">
+        <input type = "text" placeholder="Friend name... " 
+        onChange = {(event) =>{setName(event.target.value);
+        }} 
+        />
+        <input type = "number"  placeholder="Friend age..."
+        onChange = {(event) =>{setAge(event.target.value);
+        }}
+        />
+        <button onClick={addFriend}>Add Friend</button>
+        </div>   
+
+        <div className="listOfFriends">
+        {ListOfFriends.map((val)=>{
+            return (
+            <div className="friendCointainer">
+            <div className='friend'>
+            <h3>Name:{val.name}</h3>
+            <h3>Age: {val.age}</h3>
+            </div>
+            <button onClick ={()=>
+                {updateFriend(val._id);
+            }}
+            >
+            Update</button>
+            <button id ="removeButton"
+            onClick={()=>{
+                deleteFriend(val._id);
+            }}
+            >
+            X</button>
+            </div>
+            );
+        })};           
+        </div>
+        </div>
+    )
 }
 
-export default App;
+export default App
